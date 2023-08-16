707e3bd8fd8aaeccc55b0e0ca49838d9558df780
Takashi MATSUMURA <mats@coral.plala.or.jp>
2023-08-16 18:07:30 +0900

    next font

--- nextjs13-twitter/src/app/page.js
+++ nextjs13-twitter/src/app/page.js
@@ -1,7 +1,17 @@
 import Timeline from "./components/Timeline";
+import { Noto_Sans_JP } from "next/font/google";
+
+const notosansjp = Noto_Sans_JP({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
+    // <div className={notosansjp.className}>
+    //   <Timeline />
+    // </div>
     <div>
       <Timeline />
     </div>
