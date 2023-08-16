20f435785219013c805cb0c60d9a49b3be4a7494
MatsBACCANO <43158600+Takashi-Matsumura@users.noreply.github.com>
2023-08-16 08:04:55 +0000

    このカレンダーは使わないかな

--- nextjs13-twitter/src/app/calendar/page.js
+++ nextjs13-twitter/src/app/calendar/page.js
@@ -5,13 +5,33 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
+const eventStyle = (event, start, end, isSelected) => {
+  let bgColor;
+  let fontColor;
+  if (event.id === 0) {
+    bgColor = "#BF7A87";
+    fontColor = "aliceblue";
+  } else {
+    bgColor = "#386537";
+    fontColor = "black";
+  }
+  return {
+    className: "",
+    style: {
+      backgroundColor: bgColor,
+      color: fontColor,
+    },
+  };
+};
+
 const page = () => {
   const localizer = momentLocalizer(moment);
   return (
-    <div>
+    <div className="p-4">
       <Calendar
+        className="h-full"
         localizer={localizer}
-        //events={myEventsList}
+        events={eventStyle}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
